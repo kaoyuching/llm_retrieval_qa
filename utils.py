4d38a2431e14c2ef70fc95a1dba6d9a8dc80c117
doriskao <doris_kao@muenai.com>
2024-07-04 17:58:12 +0800

    ADD: filter documents by similarity score

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from langchain_core.documents import Document
 from langchain_community.vectorstores import FAISS
 from langchain_community.vectorstores.utils import DistanceStrategy
@@ -11,12 +12,14 @@ class QAChain():
         prompt_template,
         top_k: int = 10,
         return_source_documents: bool = False,
+        similarity_score_threshold: Optional[float] = None,
     ):
         self.llm_model = llm_model
         self.vector_db = vector_db
         self.prompt_template = prompt_template
         self.top_k = top_k
         self.return_source_documents = return_source_documents
+        self.threshold = similarity_score_threshold
 
     def __call__(self, question):
         query_docs = self.vector_db.similarity_search_with_score(
@@ -29,10 +32,14 @@ class QAChain():
         contexts = []
         scores = []
         for _doc, _score in query_docs:
-            doc_str.append(_doc)
-            contexts.append(_doc.page_content)
+            if self.threshold is None or _score <= self.threshold:
+                doc_str.append(_doc)
+                contexts.append(_doc.page_content)
             scores.append(_score)
-        contexts = '\n'.join(contexts)
+        if len(contexts) == 0:
+            contexts = "Found nothing in the documents"
+        else:
+            contexts = '\n'.join(contexts)
         input_prompt = self.prompt_template.format(context=contexts, question=question)
 
         res = self.llm_model.invoke(input_prompt)
