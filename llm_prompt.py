e172f98d665a81e5a6845d93119e5d83b0b6d026
kaoyuching <doris830426@gmail.com>
2024-07-29 11:06:08 +0800

    ADD: add prompt template for Microdoft Phi3 model

--- llm_prompt.py
+++ llm_prompt.py
@@ -19,6 +19,8 @@ class PromptBase(abc.ABC):
 
 class PromptLlama2(PromptBase):
     r'''
+    PromptLlama2 is used by Llama2.
+
     llama2: https://llama.meta.com/docs/model-cards-and-prompt-formats/meta-llama-2
     llama2 prompt template: https://github.com/meta-llama/llama/blob/main/llama/generation.py#L284-L395
 
@@ -46,6 +48,8 @@ class PromptLlama2(PromptBase):
     def set_system_prompt(self, system_prompt: Optional[str] = None):
         if system_prompt is not None:
             self.system_prompt = self.B_SYS + system_prompt + self.E_SYS
+        else:
+            self.system_prompt = self.B_SYS + self.system_prompt + self.E_SYS
 
     def get_template(self, instruction: str):
         full_prompt = self.B_SENT + self.B_INST + ' ' + self.system_prompt + instruction + ' ' + self.E_INST
@@ -60,6 +64,8 @@ class PromptLlama2(PromptBase):
 
 class PromptLlama3(PromptBase):
     r'''
+    PromptLlama3 is used by Llama3 and Llama3.1.
+
     llama3: https://llama.meta.com/docs/model-cards-and-prompt-formats/meta-llama-3
     llama3 template: https://github.com/meta-llama/llama3/blob/main/llama/tokenizer.py#L203
 
@@ -90,6 +96,44 @@ class PromptLlama3(PromptBase):
     def get_template(self, instruction: str):
         full_prompt = self.system_prompt + self.B_ROLE + "user" + self.E_ROLE +\
                 instruction + self.E_INPUT + self.B_ROLE + "assistant" + self.E_ROLE
+
+        input_variables = re.findall('{(\w+)}', instruction)
+
+        prompt_template = PromptTemplate(
+            input_variables=input_variables,
+            template=full_prompt,
+        )
+        return prompt_template, full_prompt
+
+
+class PromptPhi3(PromptBase):
+    r"""
+    PromptPhi3 is used by Microsoft Phi3.
+
+    Microsoft phi3: https://huggingface.co/microsoft/Phi-3-mini-4k-instruct
+
+    prompt example:
+        <|system|>
+        You are a helpful assistant.<|end|>
+        <|user|>
+        Question?<|end|>
+        <|assistant|>
+    """
+    def __init__(self):
+        self.B_SYS = "<|system|>\n"
+        self.B_ROLE = "<|user|>\n"
+        self.B_ASST = "<|assistant|>\n"
+        self.END = "<|end|>\n"
+        self.system_prompt = "You are a helpful assistant."
+
+    def set_system_prompt(self, system_prompt: Optional[str] = None):
+        if system_prompt is not None:
+            self.system_prompt = self.B_SYS + system_prompt + self.END
+        else:
+            self.system_prompt = self.B_SYS + self.system_prompt + self.END
+
+    def get_template(self, instruction: str):
+        full_prompt = self.system_prompt + self.B_ROLE + instruction + self.END + self.B_ASST
 
         input_variables = re.findall('{(\w+)}', instruction)
 
