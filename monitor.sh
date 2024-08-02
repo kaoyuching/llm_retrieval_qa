309b6db98036ab737b68967b86eba6fc936f4cee
kaoyuching <doris830426@gmail.com>
2024-08-02 09:47:48 +0800

    UPDATE: update monitor shell

--- monitor.sh
+++ monitor.sh
@@ -6,7 +6,7 @@ TotalMem=$(free -m -t | awk '/Mem:/ {print $2}')
 
 while :
 do
-    read -r PCPU PMEM <<< $(ps -ef -o user,%cpu,%mem,start,cmd | grep llm_retrievalqa_gguf.py | sort -k 4 | head -n 1 | sed "s/\s\s*/ /g" | cut -d " " -f 2,3)
+    read -r PCPU PMEM <<< $(ps -e -o user,%cpu,%mem,start,cmd | grep llm_retrievalqa_gguf.py | sort -k 4 | head -n 1 | sed "s/\s\s*/ /g" | cut -d " " -f 2,3)
     UsedMem=$(bc <<< "$TotalMem * $PMEM / 100")
 
     echo "%CPU: $PCPU%"
