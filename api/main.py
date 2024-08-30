c7c51abb3fde4ad94fe7664db848eb8a93ec5c04
kaoyuching <doris830426@gmail.com>
2024-08-30 10:30:02 +0800

    NEW: fastapi

---
+++ api/main.py
@@ -0,0 +1,103 @@
+import os
+from fastapi import FastAPI, Query
+from fastapi.responses import StreamingResponse
+
+from llm_retrieval_qa.configs import settings, model_config, vector_store_config,\
+    get_prompt_template, get_embedding_fn
+from llm_retrieval_qa.splitter import split_html
+from llm_retrieval_qa.pipeline.model_loader import load_model
+from api.routers import router
+
+
+app = FastAPI()
+app.include_router(router)
+
+# embedding
+emb_cfg = model_config["embedding_cfgs"]
+embedding_fn = get_embedding_fn(emb_cfg)
+
+# prepare prompt template
+prompt_template_fn, full_prompt_template = get_prompt_template(model_config["prompt_template"])
+
+# load model
+model = load_model(model_config, settings.quantization, settings.device)
+top_k = settings.search_topk
+
+
+# load vector db
+def load_vector_db():
+    if vector_store_config.type == "milvus":
+        from llm_retrieval_qa.vector_store.milvus import DbMilvus
+
+        vector_db = DbMilvus(
+            embedding_fn,
+            vector_store_config.uri,
+            db_name=vector_store_config.db_name,
+            collection_name=vector_store_config.collection,
+        )
+    elif vector_store_config.type == "faiss":
+        from llm_retrieval_qa.vector_store.faiss import DbFAISS
+        
+        vector_db = DbFAISS(embedding_fn, vector_store_config.uri, normalize=True)
+    return vector_db
+
+
+vector_db = load_vector_db()
+
+if model_config["format"] == "hf":
+    from transformers import AutoTokenizer
+    from llm_retrieval_qa.pipeline.streaming import QAHFStreamer, generate_response
+
+    tokenizer = AutoTokenizer.from_pretrained(model_config["model_path"])
+
+    streamer_config = {
+        'args': (tokenizer,),
+        'kwargs': model_config["streamer"]
+    }
+
+    if 'return_full_text' in llm_model_runtime_kwargs:
+        llm_model_runtime_kwargs.pop('return_full_text')
+
+    model_kwargs = dict(
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.eos_token_id,
+        **llm_model_runtime_kwargs,
+    )
+
+    qa_streaming = QAHFStreamer(
+        tokenizer,
+        model,
+        streamer_config,
+        vector_db,
+        prompt_template_fn,
+        top_k=top_k,
+        return_source_documents=False,
+        similarity_score_threshold=None,
+        model_kwargs=model_kwargs,
+        device=settings.device,
+    )
+elif model_config["format"] == "gguf":
+    from llm_retrieval_qa.pipeline.streaming import LlamaCppStreamer, generate_response
+
+    qa_streaming = LlamaCppStreamer(
+        model,
+        vector_db,
+        prompt_template_fn,
+        streamer_cfg=model_config["streamer"],
+        top_k=top_k,
+        return_source_documents=False,
+        similarity_score_threshold=None,
+        model_kwargs=model_config["generate"],
+    )
+
+
+async def generator(question: str):
+    qa_streaming(question)
+    for x in qa_streaming.streamer:
+        yield x
+
+
+@app.get('/answer_stream/')
+async def stream(question: str = Query()):
+    return StreamingResponse(generator(question), media_type="text/event-stream")
---
+++ api/routers.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, UploadFile
+
+from llm_retrieval_qa.splitter import split_html
+from llm_retrieval_qa.configs import model_config, get_embedding_fn, vector_store_config
+
+
+router = APIRouter()
+
+embedding_fn = get_embedding_fn(model_config["embedding_cfgs"])
+
+
+@router.post("/upload_doc/")
+async def upload_doc(
+    file: UploadFile,
+    chunksize: int = 500,
+    chunk_overlap: int = 30,
+    vector_store_type: str = 'faiss',
+):
+    filename = file.filename
+    contents = file.read()
+
+    headers_to_split_on = [
+        ("h1", "Header 1"),
+        ("h2", "Header 2"),
+        ("h3", "Header 3"),
+        ("h4", "Header 4"),
+        ("h5", "Header 5"),
+        ("table", 'table'),
+    ]
+
+    splits = split_html(
+        contents,
+        encoding='utf-8',
+        sections_to_split=headers_to_split_on,
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=["\n\n", "\n", ",", "."],
+    )
+
+    # store data to vector database
+    if vector_store_type == "milvus":
+        from llm_retrieval_qa.vector_store.milvus import DbMilvus
+
+        vector_db = DbMilvus(
+            embedding_fn,
+            vector_store_config.uri,
+            db_name=vector_store_config.db_name,
+            collection_name=vector_store_config.collection,
+        )
+
+        texts = [x.dict()["page_content"] for x in splits]
+        # check if the doc is already in the vector db using doc_fname
+        exist_docs = vector_db.get(f'doc_fname == "{doc_fname}"')
+        if len(exist_docs) == 0:
+            _ = vector_db.create(texts, doc_fname=doc_fname)
+    elif vector_store_type == "faiss":
+        from llm_retrieval_qa.vector_store.faiss import DbFAISS
+
+        vector_db = DbFAISS(embedding_fn, vector_store_config.uri, normalize=True)
+
+        if not vector_db.check_by_doc_fname(doc_fname) or doc_fname == "default":
+            texts = [x.dict()["page_content"] for x in splits]
+            _ = vector_db.create(texts, doc_fname=doc_fname)
+            vector_db.save_local(vector_store_config.uri)
+    else:
+        vector_db = None
+    return {"documents": splits, 'vector_db': vector_db}
